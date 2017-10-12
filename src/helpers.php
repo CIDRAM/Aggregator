955174b151de915f8f93da24bda8867489e8d40f
André Filipe <andre.r.flip@gmail.com>
2017-10-12 12:01:18 +0100

    Added helper function

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('aggregate')) {
+    /**
+     * Aggregate it!
+     *
+     * @param string $input
+     *
+     * @return string
+     */
+    function aggregate($input)
+    {
+        return (new \CIDRAM\Aggregator\Aggregator())->aggregate($input);
+    }
+}
