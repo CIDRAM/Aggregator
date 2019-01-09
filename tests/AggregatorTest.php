604bd77e3b0f347c1d5b8b811fa67d2dcd5c71ef
Caleb Mazalevskis <maikuolan@gmail.com>
2019-01-09 18:05:05 +0800

    v1.2.0
    Prepare tagged release.

--- tests/AggregatorTest.php
+++ tests/AggregatorTest.php
@@ -5,7 +5,9 @@ use PHPUnit\Framework\TestCase;
 
 class AggregatorTest extends TestCase
 {
-    public function testInOut()
+
+    /** Test Aggregator using CIDR notation as output. */
+    public function testInOutCIDRs()
     {
         $TestInput = '127.0.0.1 Some arbitrary single IPs from here
 127.0.0.2
@@ -68,6 +70,77 @@ QWEQWEQWE';
 1:2:3:4::2/127
 2002::1/128';
         $Aggregator = new Aggregator();
+        $Aggregator->Results = true;
+        $Aggregated = $Aggregator->aggregate($TestInput);
+        $ExpectedOutput = str_replace(PHP_EOL, "\n", $ExpectedOutput);
+
+        $this->assertEquals($ExpectedOutput, $Aggregated, 'Actual aggregated output does not match expected aggregated output!');
+    }
+
+    /** Test Aggregator using netmask notation as output. */
+    public function testInOutNetmasks()
+    {
+        $TestInput = '127.0.0.1 Some arbitrary single IPs from here
+127.0.0.2
+127.0.0.3
+1::
+1::1
+1:2:3:4::0
+1:2:3:4::1
+1:2:3:4::2
+1:2:3:4::3
+2002::1
+127.0.0.4
+127.0.0.5
+257.0.0.999 Some arbitrary INVALID single IPs from here
+555.666.777.888
+2002:abcd:efgh::1
+10.0.0.0/9 Some arbitrary CIDRs from here
+10.128.0.0/9
+10.192.0.0/10
+11.128.0.0/10
+11.192.0.0/10
+12.0.0.0/9
+12.128.0.0/9
+13.0.0.0/9
+13.128.0.0/9
+192.168.0.0/8 Some arbitrary INVALID CIDRs from here
+192.168.0.0/9
+192.168.0.0/10
+192.168.192.0/10
+192.169.0.0/10
+192.169.64.0/10
+1.2.3.4/255.255.255.254 Some arbitrary netmasks from here
+2.3.4.5/255.255.255.255
+99.99.99.99/255.255.255.255
+99.10.10.0/255.255.255.0
+99.10.11.0/255.255.255.0
+99.8.0.0/255.252.0.0
+11.11.11.11/11.11.11.11 Some arbitrary INVALID netmasks from here
+255.255.255.254/1.2.3.4
+6.7.8.9/255.255.255.254
+88.88.88.88/255.255.254.255
+Foobar Some garbage data from here
+ASDFQWER!@#$
+>>HelloWorld<<
+SDFSDFSDF
+QWEQWEQWE';
+        $ExpectedOutput = '1.2.3.4/255.255.255.254
+2.3.4.5/255.255.255.255
+10.0.0.0/255.0.0.0
+11.128.0.0/255.128.0.0
+12.0.0.0/254.0.0.0
+99.8.0.0/255.252.0.0
+99.10.10.0/255.255.254.0
+99.99.99.99/255.255.255.255
+127.0.0.1/255.255.255.255
+127.0.0.2/255.255.255.254
+127.0.0.4/255.255.255.254
+1::/ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffe:0
+1:2:3:4::1/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:0
+1:2:3:4::2/ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffe:0
+2002::1/ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:0';
+        $Aggregator = new Aggregator(1);
         $Aggregator->Results = true;
         $Aggregated = $Aggregator->aggregate($TestInput);
         $ExpectedOutput = str_replace(PHP_EOL, "\n", $ExpectedOutput);
