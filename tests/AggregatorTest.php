fec38f7e230b1ae0eed356dfcc4f452d1c822695
Caleb Mazalevskis <maikuolan@gmail.com>
2019-01-09 17:51:09 +0800

    Update tests.
    - Aggregator now sorts its input differently, as such that IPv4 and IPv6
      output isn't thoroughly mixed together anymore.
    - Aggregator now supports netmasks, so we need to test for this now.

--- tests/AggregatorTest.php
+++ tests/AggregatorTest.php
@@ -37,20 +37,36 @@ class AggregatorTest extends TestCase
 192.168.192.0/10
 192.169.0.0/10
 192.169.64.0/10
+1.2.3.4/255.255.255.254 Some arbitrary netmasks from here
+2.3.4.5/255.255.255.255
+99.99.99.99/255.255.255.255
+99.10.10.0/255.255.255.0
+99.10.11.0/255.255.255.0
+99.8.0.0/255.252.0.0
+11.11.11.11/11.11.11.11 Some arbitrary INVALID netmasks from here
+255.255.255.254/1.2.3.4
+6.7.8.9/255.255.255.254
+88.88.88.88/255.255.254.255
 Foobar Some garbage data from here
 ASDFQWER!@#$
 >>HelloWorld<<
 SDFSDFSDF
 QWEQWEQWE';
-        $ExpectedOutput = '1::/127
-1:2:3:4::/126
+        $ExpectedOutput = '1.2.3.4/31
+2.3.4.5/32
 10.0.0.0/8
 11.128.0.0/9
 12.0.0.0/7
-2002::1/128
+99.8.0.0/14
+99.10.10.0/23
+99.99.99.99/32
 127.0.0.1/32
 127.0.0.2/31
-127.0.0.4/31';
+127.0.0.4/31
+1::/127
+1:2:3:4::1/128
+1:2:3:4::2/127
+2002::1/128';
         $Aggregator = new Aggregator();
         $Aggregator->Results = true;
         $Aggregated = $Aggregator->aggregate($TestInput);
